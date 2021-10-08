e0e4cabb93a20a625f8387d2e427ab1082c1074f
Jack Short <john.short.tj@gmail.com>
2021-10-08 12:51:09 -0400

    iniital

---
+++ Raycast/git-save-changes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Note: Set currentDirectoryPath to your local repository.
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Save Changes
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ./images/git.png
+# @raycast.packageName Git
+# @raycast.argument1 { "type": "text", "placeholder": "Message", "optional": true }
+# @raycast.currentDirectoryPath ~/Documents/Waitless
+
+# Documentation:
+# @raycast.author Thomas Paul Mann
+# @raycast.authorURL https://github.com/thomaspaulmann
+# @raycast.description Commit all pending changes. If no commit message is provided, it will amend the changes.
+
+STATUS=$(git status --short)
+
+if [ -z "$STATUS" ]; then
+  echo "No changes to save"
+  exit 1
+fi
+
+if [ -n "$1" ]; then
+  git add .
+  git commit --all --message "$1"
+  echo "Committed changes"
+else
+  git add .
+  git commit -all --amend --no-edit
+  echo "Amended changes"
+fi
